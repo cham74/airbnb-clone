b40a53a61106f623db1d8509005f431a7e5737fe
Jacob Lutin <jacobl1933@gmail.com>
2019-04-19 10:59:07 +0300

    use reactivesearch components, airbase elastic server

--- src/App.js
+++ src/App.js
@@ -1,28 +1,94 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { ReactiveBase, NumberBox, DateRange, RangeSlider, ResultCard } from '@appbaseio/reactivesearch';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+export default () => (
+    <div className="container">
+        <ReactiveBase
+            app="airbeds-demo"
+            credentials="qkm4tKcqf:3d44a96d-ca12-431d-9383-18f4c6f6f054"
+            type="listing"
+            theme={{
+                primaryColor: '#FF3A4E',
+            }}
+        >
+            <nav className="nav">
+                <div className="title">airbeds</div>
+            </nav>
+            <div className="left-col">
+                <DateRange
+                    dataField="date_from"
+                    componentId="DateRangeSensor"
+                    title="When"
+                    numberOfMonths={2}
+                    queryFormat="basic_date"
+                    initialMonth={new Date('04-01-2019')}
+                />
 
-export default App;
+                <NumberBox
+                    componentId="GuestSensor"
+                    dataField="accommodates"
+                    title="Guests"
+                    defaultSelected={2}
+                    labelPosition="right"
+                    data={{
+                        start: 1,
+                        end: 16,
+                    }}
+                />
+
+                <RangeSlider
+                    componentId="PriceSensor"
+                    dataField="price"
+                    title="Price Range"
+                    range={{
+                        start: 10,
+                        end: 250,
+                    }}
+                    rangeLabels={{
+                        start: '$10',
+                        end: '$250',
+                    }}
+                    defaultSelected={{
+                        start: 10,
+                        end: 50,
+                    }}
+                    stepValue={10}
+                    interval={20}
+                    react={{
+                        and: ['DateRangeSensor'],
+                    }}
+                />
+            </div>
+
+            <ResultCard
+                className="right-col"
+                componentId="SearchResult"
+                dataField="name"
+                size={12}
+                onData={data => ({
+                    image: data.picture_url,
+                    title: data.name,
+                    description: (
+                        <div>
+                            <div className="price">${data.price}</div>
+                            <p className="info">{data.room_type} · {data.accommodates} guests</p>
+                        </div>
+                    ),
+                    url: data.listing_url,
+                })}
+                pagination
+                react={{
+                    and: ['GuestSensor', 'PriceSensor', 'DateRangeSensor', 'search'],
+                }}
+                innerClass={{
+                    resultStats: 'result-stats',
+                    list: 'list',
+                    listItem: 'list-item',
+                    image: 'image',
+                }}
+            />
+        </ReactiveBase>
+    </div>
+);
